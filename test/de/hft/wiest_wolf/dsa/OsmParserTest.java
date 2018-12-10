3254b2e027b8b153fc6b1f69a723e90861052913
ewolf34 <erik.wolf@fuenf-woelfe.de>
2018-12-10 09:03:46 +0100

    added testOsmTiefensuche

--- test/de/hft/wiest_wolf/dsa/OsmParserTest.java
+++ test/de/hft/wiest_wolf/dsa/OsmParserTest.java
@@ -1,6 +1,7 @@
 package de.hft.wiest_wolf.dsa;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OsmParserTest
 {
@@ -15,6 +16,18 @@ public class OsmParserTest
         System.out.println();
 
         graph.dijkstra("3607596627");
+
+        System.out.println();
+    }
+
+    @Test
+    public void testOsmTiefensuche()
+    {
+        System.out.println("Tiefensuche");
+
+        Graph graph = OsmParser.parseToGraph("./additionalFiles/malmsheim-gartenstr-bis-schule-osmexport.osm");
+
+        assertEquals(graph.getVertexCount(),graph.tiefensuche("3607596627").size());
 
         System.out.println();
     }
